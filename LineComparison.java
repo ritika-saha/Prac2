fa9763bab06ace0f44f89daa1ccb8700f51b6fc3
ritika-saha <ritika882001@gmail.com>
2023-11-21 20:18:41 +0530

    dry

--- LineComparison.java
+++ LineComparison.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class LineComparison {
+    public double findLen(int x1,int y1,int x2,int y2){
+        return Math.sqrt(Math.pow((x2-x1), 2)+Math.pow((y2-y1),2));
+    }
     public static void main(String[] args){
         System.out.println("Welcome to Line Comparison Computation Program");
 
@@ -12,7 +15,8 @@ public class LineComparison {
         System.out.println("Enter coordinates of point 2: ");
         int x2=sc.nextInt();
         int y2=sc.nextInt();
-        double len=Math.sqrt(Math.pow((x2-x1), 2)+Math.pow((y2-y1),2));
+        LineComparison lc = new LineComparison();
+        double len=lc.findLen(x1,y1,x2,y2);
         System.out.println("Length of the line is : "+len+" units");
     }
 }
