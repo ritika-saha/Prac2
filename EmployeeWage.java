4d4833f0923078bd85b6a1921153bc61c12a9a95
ritika-saha <ritika882001@gmail.com>
2023-11-21 18:58:15 +0530

    Welcome message

---
+++ EmployeeWage.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class EmployeeWage {
+    public static void main(String[] args){
+        System.out.println("Welcome to Employee\r\n" + //
+                "Wage Computation\r\n" + //
+                "Program-----------------------------------------------------------------");
+    }
+    
+}
