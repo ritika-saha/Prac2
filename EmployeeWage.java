5f7283f69dc3bfaae1c2ee4fb9aa6996ca83de53
ritika-saha <ritika882001@gmail.com>
2023-11-21 19:08:27 +0530

    attendance check

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,6 +5,14 @@ public class EmployeeWage {
         System.out.println("Welcome to Employee\r\n" + //
                 "Wage Computation\r\n" + //
                 "Program-----------------------------------------------------------------");
+
+                //uc1
+                int attendance= (int)Math.floor(Math.random()*10)%2;
+               // System.out.println(attendance);
+                if(attendance==1)
+                System.out.println("Employee is present");
+                else
+                System.out.println("Employee is absent");
     }
     
 }
