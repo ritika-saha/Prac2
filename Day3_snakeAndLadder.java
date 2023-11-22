711f3e0059e02b398d82bbe9136e36604fba0c68
ritika-saha <ritika882001@gmail.com>
2023-11-22 12:43:04 +0530

    single player final

--- Day3_snakeAndLadder.java
+++ Day3_snakeAndLadder.java
@@ -10,42 +10,40 @@ class gamePlay {
          Random random = new Random();
         int randomNumber = random.nextInt(( 6- 1) + 1) + 1;
         diceRollNum++;
-        System.out.println("current Position: "+position);
         return randomNumber;
     }
-    void play(){
-        int moves=diceRoll();
-         Random random = new Random();
-        int option = random.nextInt(( 3- 1) + 1) + 1;
-        if(option==1){
-            System.out.println("No Moves");
-        return;
-        }
-        else if(option==2){
-            System.out.println("Ladder !");
-            if(position+moves<=100)
-            position+=moves;
-        }
-        else if(option==3){
-            System.out.println("Snake !");
-            if(position-moves<0)
-            position=0;
-            else
-            position-=moves;
-        }
+  void play() {
+    int moves = diceRoll();
+    Random random = new Random();
+    int option = random.nextInt((3 - 1) + 1) + 1;
+    if (option == 1) {
+        System.out.println("-------No Moves------");
+    } else if (option == 2) {
+        System.out.println("------Ladder!-----");
+        if (position + moves <= 100)
+            position += moves;
+    } else if (option == 3) {
+        System.out.println("------Snake!-------");
+        if (position - moves < 0)
+            position = 0;
+        else
+            position -= moves;
     }
+    System.out.println("Final Position: " + position);
+}
+
 
 
 }
 public class Day3_snakeAndLadder {
 
     public static void main(String[] args){
-        //for single player
+        //for single player singlePlayer_SnL branch
         int position=0;
         gamePlay gp=new gamePlay(position);
-        while(gp.position!=100){
+        while(gp.position<100){
             gp.play();
-        }
+         }
         
         System.out.println("Number of dice rolls :"+gp.diceRollNum);
     }
