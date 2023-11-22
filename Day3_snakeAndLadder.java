4640291a1f8df4e695fa49fdb25dc18d6dc871b4
ritika-saha <ritika882001@gmail.com>
2023-11-22 12:57:05 +0530

    double player

--- Day3_snakeAndLadder.java
+++ Day3_snakeAndLadder.java
@@ -39,12 +39,36 @@ public class Day3_snakeAndLadder {
 
     public static void main(String[] args){
         //for single player
-        int position=0;
-        gamePlay gp=new gamePlay(position);
-        while(gp.position<100){
-            gp.play();
+       // int position=0;
+        //gamePlay gp=new gamePlay(position);
+        //while(gp.position<100){
+          //  gp.play();
+        //}
+
+            //for double Player doublePlayer_SnL branch
+            int position1=0;
+            int position2=0;
+            gamePlay gp1=new gamePlay(position1);
+            gamePlay gp2=new gamePlay(position2);
+            int turn=1;
+            while(gp1.position<100 && gp2.position<100){
+                if(turn==1){
+                    turn=2;
+                    System.out.println("---------------------PLAYER 1-------------------");
+                    gp1.play();
+                }
+                 else if(turn==2){
+                    turn=1;
+                    System.out.println("---------------------PLAYER 2-------------------");
+                    gp2.play();
+                }
+            }
+        if(gp1.position==100){
+        System.out.println("WINNER IS PLAYER 1");
+        }else if(gp2.position==100){
+            System.out.println("WINNER IS PLAYER 2");
         }
-        
-        System.out.println("Number of dice rolls :"+gp.diceRollNum);
+        System.out.println("Number of dice rolls for player 1 :"+gp1.diceRollNum);
+        System.out.println("Number of dice rolls for player 1 :"+gp2.diceRollNum);
     }
 }
