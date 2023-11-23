5fc69769602f784a733d492618a72b2a74819cf8
ritika-saha <ritika882001@gmail.com>
2023-11-23 23:21:41 +0530

    edit address

--- AddressBook.java
+++ AddressBook.java
@@ -41,6 +41,66 @@ class Address{
         email.add(s);
 System.out.println(firstName.get(0)+" "+phone.get(0));
     }
+
+   // uc-3 editAddress
+void editAddress() {
+    System.out.println("Enter the name of the person whose data you want to edit");
+    String n = sc.nextLine();
+    System.out.println("Enter the field you want to change");
+    String field = sc.nextLine();
+    int ind = 0;
+    for (int i = 0; i < firstName.size(); i++) {
+        if (firstName.get(i).equals(n)) {  // Use equals for string comparison
+            ind = i;
+            break;
+        }
+    }
+    if (field.equals("firstName")) {  // Use equals for string comparison
+        System.out.println("Enter new first Name");
+        String nn = sc.nextLine();
+        firstName.set(ind, nn);
+    } else if (field.equals("lastName")) {
+        System.out.println("Enter new Last name");
+        String nn = sc.nextLine();
+        lastName.set(ind, nn);
+    } else if (field.equals("address")) {
+        System.out.println("Enter new address");
+        String nn = sc.nextLine();
+        address.set(ind, nn);
+    } else if (field.equals("city")) {
+        System.out.println("Enter new city");
+        String nn = sc.nextLine();
+        city.set(ind, nn);
+    } else if (field.equals("state")) {
+        System.out.println("Enter new State");
+        String nn = sc.nextLine();
+        state.set(ind, nn);
+    } else if (field.equals("zip")) {
+        System.out.println("Enter new zip");
+        String nn = sc.nextLine();
+        zip.set(ind, nn);
+    } else if (field.equals("phone")) {
+        System.out.println("Enter new phone number");
+        String nn = sc.nextLine();
+        phone.set(ind, nn);
+    } else if (field.equals("email")) {
+        System.out.println("Enter new email");
+        String nn = sc.nextLine();
+        email.set(ind, nn);
+    }
+}
+
+
+    void display(){
+        System.out.println("Name\taddress\tcity\tstate\tzip\tphone\temail");
+        System.out.println("----------------------------------------------------------------------------------------------");
+        for(int i=0;i<firstName.size();i++){
+            System.out.println(firstName.get(i)+" "+lastName.get(i)+"\t"+address.get(i)+"\t"+city.get(i)+"\t"+state.get(i)+"\t"+zip.get(i)+"\t"+phone.get(i)+"\t"+email.get(i));
+
+        }
+    }
+
+    
 }
 
 public class AddressBook {
@@ -49,6 +109,14 @@ public class AddressBook {
 
         Address ob=new Address();
         ob.addNewAddress();
+        ob.display();
+        System.out.println("do you want to edit? yes/no");
+        Scanner scan=new Scanner(System.in);
+        String ss=scan.nextLine();
+        if(ss.equals("yes")){
+            ob.editAddress();
+            ob.display();
+        }
 
     }
 }
