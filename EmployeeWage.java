473cf98be337f72d597d8bc4fc2d55ef6b277223
ritika-saha <ritika882001@gmail.com>
2023-11-23 14:06:11 +0530

    uc-7

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,5 +1,41 @@
 import java.util.Scanner;
+/**
+ * wage
+ */ 
+ class wage {
+  String employeeType;
+  int dailyWage;
+  int monthlyWage;
+  wage(String et){
+    employeeType=et;
+    dailyWage=0;
+    monthlyWage=0;
+  }
+  void calcWage(){
+    switch (employeeType) {
+                    case "yes": System.out.println("Employee is Fulltime");
+                    dailyWage=20*8;
+                        
+                        break;
+                    case "no": System.out.println("Employee is part-time");
+                    dailyWage=20*4;
+                    break;
+                
+                    default:
+                    System.out.println("invalid");
+                        break;
+                }
+    monthlyWage=20*dailyWage;
 
+  }
+
+  void displayWage(){
+    System.out.println("daily Wage is Rs: "+dailyWage);
+    System.out.println("Monthly Wage is Rs: "+monthlyWage);
+  }
+  
+    
+}
 public class EmployeeWage {
     public static void main(String[] args){
         System.out.println("Welcome to Employee\r\n" + //
@@ -49,6 +85,17 @@ public class EmployeeWage {
                     finalWage+=dailyWage;
                 }
                 System.out.println("wage after 20 days : Rs. "+finalWage);
+
+
+                //uc-7 branch name uc7_employeeWage
+                wage ob=new wage(s);
+                ob.calcWage();
+                ob.displayWage();
+
+
+
+
+                sc.close();
 
     }
     
