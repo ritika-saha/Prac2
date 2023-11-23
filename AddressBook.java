b7bcbd6185a3b87dfe5d2ccd1706f6e268573273
ritika-saha <ritika882001@gmail.com>
2023-11-23 22:47:18 +0530

    add

--- AddressBook.java
+++ AddressBook.java
@@ -12,13 +12,43 @@ class Address{
     ArrayList<String> zip = new ArrayList<>();
     ArrayList<String> phone = new ArrayList<>();
     ArrayList<String> email = new ArrayList<>();
-
-    
+    Scanner sc=new Scanner(System.in);
+    //uc-2 branch addAddress
+    void addNewAddress(){
+        System.out.println("Enter First name");
+        String s=sc.nextLine();
+        firstName.add(s);
+        System.out.println("Enter Last name");
+        s=sc.nextLine();
+        lastName.add(s);
+        System.out.println("Enter address");
+        s=sc.nextLine();
+        address.add(s);
+        System.out.println("Enter city");
+        s=sc.nextLine();
+        city.add(s);
+        System.out.println("Enter state");
+        s=sc.nextLine();
+        state.add(s);
+        System.out.println("Enter zip");
+        s=sc.nextLine();
+        zip.add(s);
+        System.out.println("Enter Phone number");
+        s=sc.nextLine();
+        phone.add(s);
+        System.out.println("Enter email");
+        s=sc.nextLine();
+        email.add(s);
+System.out.println(firstName.get(0)+" "+phone.get(0));
+    }
 }
 
 public class AddressBook {
-    public static void AddressBookMain(String[] args){
+    public static void main(String[] args){
         System.out.println("----------------Welcome to Address Book Program--------------");
+
+        Address ob=new Address();
+        ob.addNewAddress();
 
     }
 }
