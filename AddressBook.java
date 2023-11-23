5d4da3b0f12029fd1b2c651ee80735dce9d1f690
ritika-saha <ritika882001@gmail.com>
2023-11-23 22:27:11 +0530

    welcome to addressBook

---
+++ AddressBook.java
@@ -0,0 +1,6 @@
+public class AddressBook {
+    public static void AddressBookMain(String[] args){
+        System.out.println("----------------Welcome to Address Book Program--------------");
+
+    }
+}
