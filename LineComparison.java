32d0182951f3cfd71cd1d0a1af0ad4c8d9dc658a
ritika-saha <ritika882001@gmail.com>
2023-11-23 15:13:42 +0530

    using oops

--- LineComparison.java
+++ LineComparison.java
@@ -1,5 +1,44 @@
 import java.util.Scanner;
+class comparison{
+    int x1,x2,y1,y2;
+    double len;
+    comparison(int a,int b,int c,int d){
+        x1=a;
+        y1=b;
+        x2=c;
+        y2=d;
+        len=0.0;
+    }
+    double findLen(){
+          len=Math.sqrt(Math.pow((x2-x1), 2)+Math.pow((y2-y1),2));
+        System.out.println("length of line ("+x1+","+y1+") and ("+x2+","+y2+") is : "+len+" units");
+        return len;
+    }
+    boolean isEqual(double l1,double l2){
+        return l1==l2;
+    }
+    void comp(double line1,double line2){
+        int comp=Double.compare(line1, line2);
+         if(comp==0)
+         System.out.println("both have equal lengths---------");
+         else if(comp<0)
+         System.out.println("Line 1 is smaller than line 2--------");
+         else
+         System.out.println("Line 1 is greater than line 2----------");
+    }
+    
+    void main(comparison ob1,comparison ob2){
+        boolean check=isEqual(ob1.len, ob2.len);
+        if(check)
+         System.out.println("Both of them are of same length that is -----"+ob1.len);
+         else
+         System.out.println("Not equal--------");
+         comp(ob1.len, ob2.len);
+        
+    }
 
+
+}
 public class LineComparison {
     public double findLen(int x1,int y1,int x2,int y2){
         return Math.sqrt(Math.pow((x2-x1), 2)+Math.pow((y2-y1),2));
@@ -58,6 +97,14 @@ public class LineComparison {
          System.out.println("Line 1 is smaller than line 2");
          else
          System.out.println("Line 1 is greater than line 2");
+
+         //uc4- branch name OOPlineCompare
+         comparison c1=new comparison(2, 4, 5, 6);
+         comparison c2=new comparison(4, 6, 8, 1);
+         c1.findLen();
+         c2.findLen();
+         c1.main(c1, c2);;
+
 
          sc.close();
     }
