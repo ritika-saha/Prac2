9a03a1bfe8ac9a74ddc186033eff31144d7129fe
ritika-saha <ritika882001@gmail.com>
2023-11-23 22:33:16 +0530

    create address

--- AddressBook.java
+++ AddressBook.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+//first and last names, address,city, state, zip, phone number and email...
+class Address{
+    ArrayList<String> firstName = new ArrayList<>();
+    ArrayList<String> lastName = new ArrayList<>(); 
+    ArrayList<String> address = new ArrayList<>();
+    ArrayList<String> city = new ArrayList<>();
+    ArrayList<String> state = new ArrayList<>();
+    ArrayList<String> zip = new ArrayList<>();
+    ArrayList<String> phone = new ArrayList<>();
+    ArrayList<String> email = new ArrayList<>();
+
+    
+}
+
 public class AddressBook {
     public static void AddressBookMain(String[] args){
         System.out.println("----------------Welcome to Address Book Program--------------");
